42197f34cbff7d042bbe2faeddccb99e14908e03
Bernd Porr <mail@berndporr.me.uk>
2025-12-10 22:16:03 +0000

    Made the demo more readable.

--- demo_mobilenet.cpp
+++ demo_mobilenet.cpp
@@ -2,7 +2,10 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-std::vector<std::string> loadLabels(std::string filename)
+const torch::Device device = torch::kCPU; // change to torch::kCUDA if available
+
+// loads the labels into a vector
+const std::vector<std::string> loadLabels(const std::string filename)
 {
     std::vector<std::string> lines;
     std::ifstream input(filename);
@@ -13,6 +16,30 @@ std::vector<std::string> loadLabels(std::string filename)
     return lines;
 }
 
+int inference(MobileNetV2& model, std::string path) {
+    // load the image from disk
+    cv::Mat img = cv::imread(path.c_str());
+    if (img.empty()) throw std::invalid_argument("Cannot load image.");
+
+    // scale and crop the image.
+    torch::Tensor input = model.preprocess(img);
+
+    // uploads the image to the device (CPU or GPU)
+    input = input.to(device);
+
+    // turn the image into a batch containing one image
+    input = input.unsqueeze(0);
+
+    // do the inference
+    torch::Tensor output = model.forward(input);
+
+    // getting rid of the batch and obtaining an array of label scores
+    output = output.squeeze();
+
+    // finding the label with the highest score and return its index
+    return output.argmax(0).item<int>();
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
@@ -21,30 +48,20 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    auto labels = loadLabels("labels.txt");
+    // getting the text labels
+    const auto labels = loadLabels("labels.txt");
 
-    torch::manual_seed(1);
-    torch::Device device = torch::kCPU; // change to torch::kCUDA if available
-
+    // create an instance of MobilenetV2
     MobileNetV2 model;
+    // load the pre-trained weights
     model.load_weights("mobilenet_v2.pt");
+    // upload it all to the device (CPU or GPU)
     model.to(device);
+    // switch to pure inference so no training
     model.eval();
 
-    cv::Mat img = cv::imread(argv[1]);
-    if (img.empty())
-    {
-        std::cerr << "Failed to open the image.\n";
-        return -1;
-    }
-    torch::Tensor input = model.preprocess(img).to(device);
-
-    input = input.unsqueeze(0);
-    torch::Tensor output = model.forward(input);
-    output = output.squeeze();
-
-    auto idx = output.argmax(0).item<int>();
-    std::cout << "Predicted class " << idx << ": " << labels[idx] << std::endl;
-
+    // do the inference by loading an image and getting the index of the label
+    const int idx = inference(model,argv[1]);
+    std::cout << "Predicted class is " << idx << ": " << labels[idx] << std::endl;
     return 0;
 }
