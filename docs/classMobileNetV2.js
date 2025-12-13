63692538a63480f597423d821ee6c94d23859804
Bernd Porr <mail@berndporr.me.uk>
2025-12-13 19:32:33 +0000

    Updated the docs

--- docs/classMobileNetV2.js
+++ docs/classMobileNetV2.js
@@ -3,7 +3,7 @@ var classMobileNetV2 =
     [ "MobileNetV2", "classMobileNetV2.html#a160e21f81382ed20b6630706bdb1945c", null ],
     [ "forward", "classMobileNetV2.html#a02be1eecd34d927cf49ba1e90ead59e3", null ],
     [ "getClassifier", "classMobileNetV2.html#a50279dc9d0194cf532aee5b1a3068029", null ],
-    [ "getNinputChannels4Classifier", "classMobileNetV2.html#a38e5697a203d231b82a52f6cb68b026e", null ],
+    [ "getNinputChannelsOfClassifier", "classMobileNetV2.html#a0b406eeb402f4adac4a3fc34d420b22e", null ],
     [ "initialize_weights", "classMobileNetV2.html#a5bbe3c9b0bd4bc1c572fbafcfccd69f2", null ],
     [ "load_weights", "classMobileNetV2.html#a4cdc7d76796f9b8283f2e9a0c0f88f52", null ],
     [ "replaceClassifier", "classMobileNetV2.html#a8a39e0d197a7f0e053357a4b1ac0d5b0", null ],
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['getclassifier_0',['getClassifier',['../classMobileNetV2.html#a50279dc9d0194cf532aee5b1a3068029',1,'MobileNetV2']]],
-  ['getninputchannels4classifier_1',['getNinputChannels4Classifier',['../classMobileNetV2.html#a38e5697a203d231b82a52f6cb68b026e',1,'MobileNetV2']]]
+  ['getninputchannelsofclassifier_1',['getNinputChannelsOfClassifier',['../classMobileNetV2.html#a0b406eeb402f4adac4a3fc34d420b22e',1,'MobileNetV2']]]
 ];
