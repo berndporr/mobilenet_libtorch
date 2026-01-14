ce43da172e4bea3380aceb2d20a64deae1721504
Bernd Porr <mail@berndporr.me.uk>
2026-01-14 18:45:39 +0000

    Moved the experimental subdir with executorch to a new github repo.
    https://github.com/berndporr/mobilenet_executorch now hosts the code
    of the experimental dir. It's working now so no longer experimental!

--- experimental/run.py
+++
@@ -1,8 +0,0 @@
-# Test locally via ExecuTorch runtime's pybind API (optional)
-import torch
-from executorch.runtime import Runtime
-pte_filename = "mobilenet_features_quant.pte"
-runtime = Runtime.get()
-method = runtime.load_program(pte_filename).load_method("forward")
-outputs = method.execute([torch.randn(1, 3, 224, 224)])
-print(outputs)
